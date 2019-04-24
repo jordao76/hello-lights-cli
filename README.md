8d4d912aae6150f291f6b37d835700e764a2bf44
Rodrigo Jordao <rodrigo.jordao@gmail.com>
2019-04-24 17:39:18 -0400

    hello-lights-cli: execute a single command

---
+++ README.md
@@ -0,0 +1,40 @@
+# hello-lights-cli
+
+[![License](http://img.shields.io/:license-mit-blue.svg)](https://github.com/jordao76/hello-lights-cli/blob/master/LICENSE.md)
+
+> Command-line interface to control a traffic light
+
+Works with a [Cleware USB traffic light](http://www.cleware.info/data/usbtischampel_E.html).
+
+This is command-line interface (CLI) for [hello-lights](https://github.com/jordao76/hello-lights).
+
+## Install
+
+### (now)
+Git clone, cd into it, then:
+```sh
+$ npm install
+```
+### (future)
+```sh
+$ npm install -g hello-lights-cli
+```
+
+## Usage
+
+### (now)
+```sh
+$ node cli.js --help
+```
+### (future)
+```sh
+$ hello-lights --help
+```
+
+## Commands
+
+Check out the available commands [here](https://jordao76.github.io/hello-lights).
+
+## License
+
+Licensed under the [MIT license](https://github.com/jordao76/hello-lights-cli/blob/master/LICENSE.md).
---
+++ cli.js
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+
+const {Commander} = require('hello-lights');
+const chalk = require('chalk');
+
+/////////////////////////////////////////////////////////////////
+
+const logger = {
+  log: (...args) => {
+    console.log(chalk.gray(...args));
+  },
+  error: (...args) => {
+    console.error(chalk.red(...args));
+  }
+};
+
+/////////////////////////////////////////////////////////////////
+
+require('yargs')
+  .option('device', {
+    alias: 'd',
+    describe: 'device type to use',
+    choices: ['cleware'],
+    default: 'cleware',
+    hidden: true }) // un-hide when more than one option exists
+  .option('device-path', {
+    alias: 'p',
+    describe: 'device type path to use, overrides --device',
+    normalize: true,
+    hidden: true }) // hide advanced option
+  .option('selector', {
+    alias: 's',
+    describe: 'selector type to use',
+    choices: ['single', 'multi'],
+    default: 'single' })
+  .command('exec <cmd>', 'executes a command',
+    yargs => yargs.positional('cmd', { describe: 'command to execute' }),
+    argv => exec(argv, argv.cmd, argv._.slice(1))) // argv._ includes 'exec' at index 0
+  .example('$0 exec bounce 300', '# executes the `bounce 300` command')
+  .demandCommand(1, 'What is your command?')
+  .epilogue('for more info, check out https://github.com/jordao76/hello-lights')
+  .strict()
+  .help()
+  .parse();
+
+/////////////////////////////////////////////////////////////////
+
+function resolveDeviceManager(options) {
+  const clewareDevicePath = 'hello-lights/lib/devices/cleware-switch1';
+  let path;
+  if (options.devicePath) {
+    path = options.devicePath; // note: relative to this script file, NOT to the executing directory
+  }
+  else {
+    // for now, it is always the case that: options.device === 'cleware'
+    path = clewareDevicePath;
+  }
+  let {Manager} = require(path);
+  return Manager;
+}
+
+/////////////////////////////////////////////////////////////////
+
+function resolveSelectorCtor(options) {
+  const selectorProperty =
+    options.selector === 'multi' ? 'PhysicalMultiTrafficLightSelector'
+      : 'PhysicalTrafficLightSelector'; // options.selector === 'single'
+  return require('hello-lights').selectors[selectorProperty];
+}
+
+/////////////////////////////////////////////////////////////////
+
+function resolveCommander(options) {
+  return new Commander({
+    logger,
+    manager: resolveDeviceManager(options),
+    selectorCtor: resolveSelectorCtor(options)
+  });
+}
+
+/////////////////////////////////////////////////////////////////
+
+async function exec(options, cmd, cdr = []) {
+  let commander = resolveCommander(options);
+  cdr.unshift(cmd);
+  let command = cdr.join(' ');
+  await commander.run(command);
+  commander.close();
+}
+
+/////////////////////////////////////////////////////////////////
